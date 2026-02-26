022b85d56ee9a7313dd7bb52beeb0d0ea6bc554a
Kavya Tiwari <kavyatiwari.dev@gmail.com>
2026-02-26 17:57:45 +0530

    Refactor mindmap viewer with chapters array
    Refactored mindmap viewer functionality to use a chapters array for dynamic item creation.

--- js/mindmaps.js
+++ js/mindmaps.js
@@ -1,14 +1,35 @@
-const viewer = document.getElementById('viewer');
-const viewerImg = document.getElementById('viewerImg');
+const chapters = [
+  { title: "What is Psychology", img: "mindmaps/ch1.png" },
+  { title: "Methods of Enquiry", img: "mindmaps/ch2.png" },
+  { title: "Human Development", img: "mindmaps/ch3.png" },
+  { title: "Learning", img: "mindmaps/ch4.png" },
+  { title: "Memory", img: "mindmaps/ch5.png" },
+  { title: "Thinking", img: "mindmaps/ch6.png" },
+  { title: "Motivation & Emotion", img: "mindmaps/ch7.png" },
+  { title: "Personality", img: "mindmaps/ch8.png" }
+];
 
-document.querySelectorAll('.grid img').forEach(img => {
-  img.addEventListener('click', () => {
-    viewerImg.src = img.src;
-    viewer.classList.remove('hidden');
-  });
+const list = document.getElementById("mindmapList");
+const viewer = document.getElementById("mindmapViewer");
+const image = document.getElementById("mindmapImage");
+
+chapters.forEach(ch => {
+  const div = document.createElement("div");
+  div.className = "mindmap-item";
+  div.innerText = ch.title;
+
+  div.onclick = () => openMindmap(ch.img);
+
+  list.appendChild(div);
 });
 
-viewer.addEventListener('click', () => {
-  viewer.classList.add('hidden');
-  viewerImg.src = '';
-});
+function openMindmap(src) {
+  image.src = src;
+  viewer.classList.remove("hidden");
+  list.style.display = "none";
+}
+
+function closeMindmap() {
+  viewer.classList.add("hidden");
+  list.style.display = "flex";
+}
